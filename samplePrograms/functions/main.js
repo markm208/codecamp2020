7d6192e1bafe0efe2846c0d9b32516c7910e3a0c
Mark Mahoney <markm208@gmail.com>
2020-07-14 15:08:24 -0500

    lesson 6 update

--- samplePrograms/functions/main.js
+++ samplePrograms/functions/main.js
@@ -37,24 +37,23 @@
 // printNames(names);
 // console.log(`Avg name length: ${averageNameLength(names)} letters`);
 
-
-function printNames(allNames) {
-    for(let i = 0;i < allNames.length;i++) {
-        console.log(`Hello ${allNames[i]}`);
-    }
-    let avgNameLength = averageNameLength(allNames);
-    console.log(`Average name length: ${avgNameLength} letters`);
-}
-function averageNameLength(allNames) {
-    let retVal;
-    let totalLetters = 0;
-    for(let i = 0;i < allNames.length;i++) {
-        let currentName = allNames[i];
-        totalLetters = totalLetters + currentName.length;
-    }
-    retVal = totalLetters / allNames.length;
-    return retVal;
-}
-let names = ['Mark', 'Laura', 'Buddy', 'Patrick', 'Willy'];
-printNames(names);
+// function printNames(allNames) {
+//     for(let i = 0;i < allNames.length;i++) {
+//         console.log(`Hello ${allNames[i]}`);
+//     }
+//     let avgNameLength = averageNameLength(allNames);
+//     console.log(`Average name length: ${avgNameLength} letters`);
+// }
+// function averageNameLength(allNames) {
+//     let retVal;
+//     let totalLetters = 0;
+//     for(let i = 0;i < allNames.length;i++) {
+//         let currentName = allNames[i];
+//         totalLetters = totalLetters + currentName.length;
+//     }
+//     retVal = totalLetters / allNames.length;
+//     return retVal;
+// }
+// let names = ['Mark', 'Laura', 'Buddy', 'Patrick', 'Willy'];
+// printNames(names);
 
